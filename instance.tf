d628fad432ac8b6d15f8a2a9d3ed7e6878951fa5
MederD <freenetscaper@yahoo.com>
2020-07-27 09:38:16 -0700

    commit

--- instance.tf
+++ instance.tf
@@ -8,12 +8,35 @@ provider "aws" {
 resource "aws_instance" "terraform-ec2" {
   ami           = "ami-08f3d892de259504d"
   instance_type = "t2.micro"
+  key_name = "ubuntu_keygen"
+  subnet_id = aws_subnet.terraform.id
+  associate_public_ip_address = true
+  vpc_security_group_ids = [aws_security_group.terraform.id]
+
+
   tags = {
     Name = "terraform"
   }
 
-  network_interface {
-    network_interface_id = "${aws_network_interface.terraform.id}"
-    device_index         = 0
-  }
+  connection {
+    type = "ssh"
+    user = "ec2-user"
+    private_key = file("~/.ssh/id_rsa")
+    host = self.public_ip
+ }
+
+  provisioner "remote-exec" {
+     inline = [
+       
+
+        "sudo yum update -y",
+        "sudo yum install -y httpd.x86_64",
+        "sudo systemctl start httpd.service",
+        "sudo systemctl enable httpd.service",
+      
+
+   ]
+ }
+
+
 }
--- subnet.tf
+++ subnet.tf
@@ -1,16 +1,9 @@
 resource "aws_subnet" "terraform" {
-  vpc_id     = "${aws_vpc.terraform.id}"
+  vpc_id     = aws_vpc.terraform.id
   cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "terraform"
-  }
-}
-resource "aws_network_interface" "terraform" {
-  subnet_id   = "${aws_subnet.terraform.id}"
-  private_ips = ["10.0.1.100"]
-
-  tags = {
-    Name = "primary_network_interface"
   }
 }
